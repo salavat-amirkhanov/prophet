5af7c139b7ae95b402d7dae4900c6a3d64634467
Salavat Amirkhanov <salavat@yandex-team.ru>
2017-07-11 01:03:23 +0300

    default pickle version

--- python/setup.py
+++ python/setup.py
@@ -31,9 +31,9 @@ def build_stan_models(target_dir, models_dir=MODELS_DIR):
         target_name = '{}_growth.pkl'.format(model_type)
         with open(os.path.join(models_dir, model_name)) as f:
             model_code = f.read()
-        sm = StanModel(model_code=model_code, extra_compile_args = [])
+        sm = StanModel(model_code=model_code)
         with open(os.path.join(target_dir, target_name), 'wb') as f:
-            pickle.dump(sm, f, protocol=pickle.HIGHEST_PROTOCOL)
+            pickle.dump(sm, f)
 
 
 class BuildPyCommand(build_py):
